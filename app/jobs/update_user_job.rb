7b95a04b93fffc6389f4ee7d7174b537b4b25f1a
Federico Capaldo <federico.capaldo1@gmail.com>
2022-06-08 01:01:35 +0100

    added job body

--- app/jobs/update_user_job.rb
+++ app/jobs/update_user_job.rb
@@ -1,7 +1,10 @@
 class UpdateUserJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform(user)
+    puts "Calling Clearbit API for #{user.email}..."
+    # TODO: perform a time consuming task like Clearbit's Enrichment API.
+    sleep 2
+    puts "Done! Enriched #{user.email} with Clearbit"
   end
 end
